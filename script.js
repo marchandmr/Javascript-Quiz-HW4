f42349f8e37f406912eae36e6208cd5d0ae656e6
Matthew Marchand <marchandmr@icloud.com>
2020-11-07 14:51:03 -0600

    fixed the scoreboard so that it will display initials

--- script.js
+++ script.js
@@ -27,7 +27,17 @@ var questions = [
     question: "what kind of loop is for conditional statements?",
     options: ["if statement", "for loop", "blanket statement"],
     answer: "if statement"
-  }
+  },
+  {
+    question: "what is to end a line of code?",
+    options: [";", ",", "/"],
+    answer: ";"
+  },
+  {
+    question: "What is not fundemental for Web Development?",
+    options: ["html", "potatos", "Javascript"],
+    answer: "potatos"
+  },
 ]
 
 startEl.addEventListener("click", startTimer);
@@ -113,31 +123,40 @@ function gameOver() {
   alert("All Done!")
   timeLeft = " ";
   quizEl.innerHTML = "";
-  var inputScore = document.createElement("input");
-  quizEl.append(inputScore);
+  var initials = document.createElement("input");
+  initials.setAttribute("id", "initials")
+  quizEl.append(initials);
   var submit = document.createElement("button");
   quizEl.append(submit);
   submit.textContent = "SUBMIT";
   var gameOverText = document.createElement("p")
   quizEl.append(gameOverText);
   gameOverText.textContent = "Enter your initals and press submit to save your score";
-  localStorage.setItem('submit', JSON.stringify(submit));
-  initials = localStorage.getItem('submit');
+  // localStorage.setItem('submit', JSON.stringify(submit));
+  // initials = localStorage.getItem('submit');
 
-  submit.addEventListener("click", scores);
+  submit.addEventListener("click", saveInit);
   //submit.addEventListener("click", save)
 
 }
+
+function saveInit() {
+  var x = document.getElementById("initials").value;
+  localStorage.setItem("initials", JSON.stringify(x))
+  console.log(x);
+  scores();
+}
+
+
 
 function scores() {
   quizEl.innerHTML = " ";
   choicesEl.innerHTML = " ";
   localStorage.setItem('score', JSON.stringify(score));
   var get = localStorage.getItem('score');
-  console.log(score);
-  console.log(initials);
-  quizEl.innerHTML = initials, JSON.parse(initials);
-  quizEl.innerHTML = get, JSON.parse(get);
+  var getInit = localStorage.getItem('initials');
+  // quizEl.innerHTML = initials, JSON.parse(initials);
+  quizEl.innerHTML = JSON.parse(getInit) + " : " + get, JSON.parse(get);
   var listEl = document.createElement("ul");
   listEl.setAttribute("id", "choices");
   quizEl.append(listEl);
